a96970a3aaa95cdb30aee5cf1bd36aee1b9a966b
Alexandre Dewilde <alexandredewilde2001@gmail.com>
2023-12-22 23:50:25 +0100

    day22: rm template

--- 2023/day22.py
+++ 2023/day22.py
@@ -1,53 +1,4 @@
-import requests
-import os
-day = "22"
-year = "2023"
-
-def get_example(day,offset=0):
-	req = requests.get(f'https://adventofcode.com/{year}/day/{day}', headers={'cookie':'session='+os.environ["SESSION_AOC"]})
-	return req.text.split('<pre><code>')[offset+1].split('</code></pre>')[0]
-
-def submit(day, level, answer):
-	input(f'You are about to submit the follwing answer:\n>>>>>>>>>>>>>>>>> {answer}\nPress enter to continue or Ctrl+C to abort.')
-	data = {
-	  'level': str(level),
-	  'answer': str(answer)
-	}
-
-	response = requests.post(f'https://adventofcode.com/{year}/day/{day}/answer', headers={'cookie':'session='+os.environ["SESSION_AOC"]}, data=data)
-	if 'You gave an answer too recently' in response.text:
-		print('VERDICT : TOO MANY REQUESTS')
-	elif 'not the right answer' in response.text:
-		if 'too low' in response.text:
-			print('VERDICT : WRONG (TOO LOW)')
-		elif 'too high' in response.text:
-			print('VERDICT : WRONG (TOO HIGH)')
-		else:
-			print('VERDICT : WRONG (UNKNOWN)')
-	elif 'seem to be solving the right level.' in response.text:
-		print('VERDICT : INVALID LEVEL')
-	else:
-		print('VERDICT : OK !')
-
-
-content = ""
-file = "sample.txt"
-file = "input.txt"
-
-if not os.path.exists(file):
-	cookies = {"session": os.environ["SESSION_AOC"]}
-	content = requests.get(f"https://adventofcode.com/{year}/day/{day}/input",
-		cookies=cookies,
-		headers={'User-Agent': "Mozilla/5.0 (X11; Linux i686; rv:109.0) Gecko/20100101 Firefox/120.0"}
-	).text
-	with open("input.txt", "w") as f:
-		f.write(content)
-else:
-	with open(file) as f:
-		content = f.read()
-
-from functools import cmp_to_key
-# content = get_example(day, 0)
+content = open("input.txt").read()
 
 ans = 0
 lines = content.splitlines()
@@ -169,5 +120,3 @@ for i in range(len(coords)):
 		print(broke)
 # ans = len(coords) - len(cannot)
 print(ans)
-# submit(day, 1, ans)
-# submit(day, 2, ans)
